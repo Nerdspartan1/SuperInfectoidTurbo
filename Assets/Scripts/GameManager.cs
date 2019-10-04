99c86b1524c1744eab5c9c329002a344a6a5d67c
AntoineRR <ant.romero2@orange.fr>
2019-10-04 23:47:28 +0200

    Parralax placeholders

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GamePhase {
+        FirstMenuPhase = 1,
+        GamingPhase = 2
+    }
+
+    public static GameManager instance;
+
+    public GamePhase gamePhase;
+
+    public GameObject MenuUI;
+    public GameObject GameUI;
+    
+    public float playAreaWidth;
+    public float playAreaHeight;
+
+    public float parallaxFactor = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        gamePhase = GamePhase.FirstMenuPhase;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangePhase(GamePhase nextPhase)
+    {
+        if (nextPhase == GamePhase.GamingPhase)
+        {
+            MenuUI.SetActive(false);
+            GameUI.SetActive(true);
+        }
+        else if (nextPhase == GamePhase.FirstMenuPhase)
+        {
+            GameUI.SetActive(false);
+            MenuUI.SetActive(true);
+        }
+    }
+
+    public void LaunchGame()
+    {
+        ChangePhase(GamePhase.GamingPhase);
+    }
+}
---
+++ Assets/Scripts/ScrollingScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingScript : MonoBehaviour
+{
+    public Camera camera;
+
+    private Vector2 _parallax;
+    private Vector3 _previousCameraPosition;
+    private Vector3 _initialPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _previousCameraPosition = camera.transform.position;
+        _initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            _parallax = -camera.transform.position * (transform.position.z) / GameManager.instance.parallaxFactor;
+            transform.position = _initialPosition + new Vector3(_parallax.x, _parallax.y, 0);
+    }
+}
