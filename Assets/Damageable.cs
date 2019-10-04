55b81b8d0ead163c9a6e25715026b87157766307
AntoineRR <ant.romero2@orange.fr>
2019-10-04 21:53:51 +0200

    Add player controller & damageable script

---
+++ Assets/Damageable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Damageable : MonoBehaviour
+{
+	public int life = 10;
+	public UnityEvent OnDeath;
+
+	public void TakeDamage(int damage)
+	{
+		life -= damage;
+		if(life <= 0)
+		{
+			OnDeath.Invoke();
+		}
+	}
+}
