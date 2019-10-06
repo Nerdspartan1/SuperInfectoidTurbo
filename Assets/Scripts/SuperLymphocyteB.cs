3eefc7475a31ca0dfec1d85e8052b4d3bcba37af
AntoineRR <ant.romero2@orange.fr>
2019-10-06 12:40:26 +0200

    Add Super Lymphocyte

--- Assets/Scripts/SuperLymphocyteB.cs
+++ Assets/Scripts/SuperLymphocyteB.cs
@@ -7,17 +7,15 @@ public class SuperLymphocyteB : LymphociteB
     
     public override void Shoot()
     {
-        _timer += Time.deltaTime;
+        
+    }
 
-        if (_timer > timeBetweenShoots)
+    public void Burst()
+    {
+        for (int i = -2; i < 3; i++)
         {
-            for (int i = -2; i < 3; i++)
-            {
-                GameObject bullet = Instantiate(anticorps, transform.position, transform.rotation, GameManager.instance.Game.transform);
-                bullet.GetComponent<BulletScript>().direction = Quaternion.Euler(0, 0, i*15) * (GameManager.instance.player.transform.position - bullet.transform.position).normalized;
-            }
-            
-            _timer = 0;
+            GameObject bullet = Instantiate(anticorps, transform.position, transform.rotation, GameManager.instance.Game.transform);
+            bullet.GetComponent<BulletScript>().direction = Quaternion.Euler(0, 0, i * 15) * (GameManager.instance.player.transform.position - bullet.transform.position).normalized;
         }
     }
 }
