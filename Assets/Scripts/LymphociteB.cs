bdc3e46c3b40414c15a807cb3173aa618093fdeb
Olivier <o.cornet2@gmail.com>
2019-10-06 13:36:57 +0200

    Add lymphocyte death anim

--- Assets/Scripts/LymphociteB.cs
+++ Assets/Scripts/LymphociteB.cs
@@ -15,30 +15,39 @@ public class LymphociteB : MonoBehaviour
     protected float _timer;
     public float timeBetweenShoots = 1.0f;
 
+	private bool _isDead = false;
+	private Animator _animator;
+	private SpriteRenderer _spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
         _timer = 0;
         _rigidBody = gameObject.GetComponent<Rigidbody2D>();
         _rigidBody.freezeRotation = true;
-    }
+		_animator = GetComponent<Animator>();
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+
+	}
 
     // Update is called once per frame
     void Update()
     {
-
-        if ((GameManager.instance.player.transform.position - transform.position).magnitude < attackRange) //attack range
-        {
-            gameObject.GetComponent<Animator>().SetBool("isAttacking", true);
-            Shoot();
-        }
-        else
-        {
-            gameObject.GetComponent<Animator>().SetBool("isAttacking", false);
-            _timer = 0;
+		if (!_isDead)
+		{
+			if ((GameManager.instance.player.transform.position - transform.position).magnitude < attackRange) //attack range
+			{
+				_animator.SetBool("isAttacking", true);
+				Shoot();
+			}
+			else
+			{
+				_animator.SetBool("isAttacking", false);
+				_timer = 0;
+			}
+			// Move
+			_rigidBody.velocity = speed * (GameManager.instance.player.transform.position - gameObject.transform.position);
 		}
-		// Move
-		_rigidBody.velocity = speed * (GameManager.instance.player.transform.position - gameObject.transform.position);
 	}
 
     public virtual void Shoot()
@@ -55,6 +64,11 @@ public class LymphociteB : MonoBehaviour
 
     public void Die()
     {
+		_isDead = true;
+		_animator.SetBool("isAttacking", false);
+		_animator.SetBool("dead", true);
+		_rigidBody.freezeRotation = false;
+
         EnemiesManager.numberOfEnemies--;
         EnemiesManager.numberOfKillsBeforeSuperLymphocyte--;
 		int chance = Random.Range(0, 3);
@@ -62,6 +76,24 @@ public class LymphociteB : MonoBehaviour
 		{
 			Instantiate(HealObjectPrefab, transform.position + Random.onUnitSphere * 3, Quaternion.identity, GameManager.instance.Game.transform);
 		}
-		Destroy(gameObject);
+
+		StartCoroutine(Disappear());
     }
+
+	public IEnumerator Disappear()
+	{
+		float timeBeforeDisappear = 2.0f;
+
+		while (timeBeforeDisappear > 0.0f)
+		{
+			timeBeforeDisappear -= Time.deltaTime;
+			if (timeBeforeDisappear < 1.0f)
+			{
+				_spriteRenderer.color = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, timeBeforeDisappear);
+			}
+			yield return null;
+		}
+
+		Destroy(gameObject);
+	}
 }
